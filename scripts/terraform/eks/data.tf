dbaf7465724811383f6543fa99aa9d12ded90a15
Babasanmi Adeyemi <babasanmiadeyemi@gmail.com>
2025-06-16 06:43:15 -0400

    Cluster Spinup 2nd-iteration

---
+++ scripts/terraform/eks/data.tf
@@ -0,0 +1,19 @@
+
+locals {
+  cluster_name = "${var.cluster_name}-${random_string.suffix.result}"
+}
+
+data "aws_availability_zones" "available" {
+  filter {
+    name   = "opt-in-status"
+    values = ["opt-in-not-required"]
+  }
+}
+
+data "aws_iam_policy" "ebs_csi_policy" {
+  arn = "arn:aws:iam::aws:policy/service-role/AmazonEBSCSIDriverPolicy"
+}
+
+
+
+  

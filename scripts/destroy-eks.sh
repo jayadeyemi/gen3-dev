1ca3e431a83053126dfcfa67ffa4ef942b36804c
Babasanmi Adeyemi <babasanmiadeyemi@gmail.com>
2025-06-22 02:49:04 -0400

    ACK controller working as expected. Tested with a simple s3 & vpc bucket creation and deletion.

--- scripts/destroy-eks.sh
+++ scripts/destroy-eks.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
+script_dir="$(pwd)"
 
 terraform state rm 'module.gen3-commons.module.gen3-eks.random_string.suffix' > /dev/null || true
 # Terraform plan
 terraform destroy -auto-approve #-refresh=false
 
 # Remove the outputs directory if it exists
-if [ -d "$PWD/outputs" ]; then
-  rm -rf $PWD/outputs
+if [ -d "${script_dir}/outputs" ]; then
+  rm -rf "${script_dir}/outputs"
 fi

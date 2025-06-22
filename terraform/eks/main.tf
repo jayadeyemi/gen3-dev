e0db11d06e33f81bdbb9bc67606eb613e236522e
Babasanmi Adeyemi <babasanmiadeyemi@gmail.com>
2025-06-22 02:56:44 -0400

    enabling dns

--- terraform/eks/main.tf
+++ terraform/eks/main.tf
@@ -18,7 +18,7 @@ module "vpc" {
   public_subnets        = var.vpc_public_subnets
   enable_nat_gateway    = true
   single_nat_gateway    = true
-  # enable_dns_hostnames  = true
+  enable_dns_hostnames  = true
 
   public_subnet_tags = {
     "kubernetes.io/role/elb" = 1

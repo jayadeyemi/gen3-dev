564a925cfaa0d9174874346e487683e4f8e84d69
Babasanmi Adeyemi <babasanmiadeyemi@gmail.com>
2025-06-22 02:55:53 -0400

    terraform-aws-vpc requires refresh=false

--- scripts/destroy-eks.sh
+++ scripts/destroy-eks.sh
@@ -3,7 +3,7 @@ script_dir="$(pwd)"
 
 terraform state rm 'module.gen3-commons.module.gen3-eks.random_string.suffix' > /dev/null || true
 # Terraform plan
-terraform destroy -auto-approve #-refresh=false
+terraform destroy -auto-approve -refresh=false
 
 # Remove the outputs directory if it exists
 if [ -d "${script_dir}/outputs" ]; then
